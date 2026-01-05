9fb30c16453f327720fd85f19673366a7f3d6b06
Kuan <h0169888@gmail.com>
2026-01-05 16:20:27 +0100

    add new entity Reindeer.cs

---
+++ 02 - Data/Cinvidad.TechnicalTest.Data/Entities/Reindeer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Convidad.TechnicalTest.Data.Entities
+{
+    public class Reindeer
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = default!;
+
+        [Required, MaxLength(20)]
+        public string PlateNumber { get; set; } = default!;
+
+        [Range(0, double.MaxValue)]
+        public double Weight { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Packets { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
