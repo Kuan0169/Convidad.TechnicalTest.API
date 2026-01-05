50235f0634e9e5693cbd1db91fbe4349b8348dbd
Kuan <h0169888@gmail.com>
2026-01-05 16:55:26 +0100

    add ReindeerServiceTest

---
+++ 99 - Tests/Convidad.TechnicalTest.Tests/Services/ReindeerServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Convidad.TechnicalTest.Data.Context;
+using Convidad.TechnicalTest.Services.SantaService;
+using Convidad.TechnicalTest.Data.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace Convidad.TechnicalTest.Tests.Services
+{
+    public class ReindeerServiceTest
+    {
+        public SantaDbContext CreateInMemoryDbContext()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<SantaDbContext>()
+                .UseSqlite(connection)
+                .Options;
+            var context = new SantaDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+    }
+}
