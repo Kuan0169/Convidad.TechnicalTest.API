8053281b275de27783613fa9c04f521b0b750f80
Kuan <h0169888@gmail.com>
2026-01-05 18:26:48 +0100

    created Reindeer entity

---
+++ 02 - Data/Cinvidad.TechnicalTest.Data/Entities/Reindeer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Convidad.TechnicalTest.Data.Entities
+{
+    public class Reindeer
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = default!;
+
+        [Required, MaxLength(20)]
+        public string PlateNumber { get; set; } = default!;
+
+        [Range(0, double.MaxValue)]
+        public double Weight { get; set; } // 單位：kg
+
+        [Range(0, int.MaxValue)]
+        public int Packets { get; set; } // 可攜帶包裹數
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
