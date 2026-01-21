a5d3576c88283606d44f2165b1f48b3e67a3c06b
Kuan <h0169888@gmail.com>
2026-01-21 13:15:10 +0100

    add CreateRouteDto

--- ClassLibrary1/DTOs/RouteDto.cs
+++ ClassLibrary1/DTOs/RouteDto.cs
@@ -1,7 +1,12 @@
 ﻿namespace Convidad.TechnicalTest.Models.DTOs;
+
 public record RouteDto(
-    Guid Id,
-    string Name,
-    string Region,
-    int CapacityPerNight
-);
+Guid Id,
+string Name,
+string Region,
+int CapacityPerNight);
+
+public record CreateRouteDto(
+string Name,
+string Region,
+int CapacityPerNight = 50);
