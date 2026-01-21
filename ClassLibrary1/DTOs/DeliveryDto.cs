17db061fe87f84a7e56091c5ae2e723713304b2f
Kuan <h0169888@gmail.com>
2026-01-21 13:16:13 +0100

    add CreateDeliveryDto

--- ClassLibrary1/DTOs/DeliveryDto.cs
+++ ClassLibrary1/DTOs/DeliveryDto.cs
@@ -1,9 +1,11 @@
-﻿namespace Convidad.TechnicalTest.Models.DTOs
-{ 
-    public record DeliveryDto(
-    Guid Id,
-    Guid ChildId,
-    Guid RouteId,
-    string Status,
-    DateTimeOffset CreatedAt);
-}
+﻿namespace Convidad.TechnicalTest.Models.DTOs;
+public record DeliveryDto(
+Guid Id,
+Guid ChildId,
+Guid RouteId,
+string Status,
+DateTimeOffset CreatedAt);
+
+public record CreateDeliveryDto(
+Guid ChildId,
+Guid RouteId);
