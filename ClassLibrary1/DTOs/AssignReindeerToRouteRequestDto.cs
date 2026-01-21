eefa9daf25dc6950a481560a39ebadb00e9b445d
Kuan <h0169888@gmail.com>
2026-01-21 19:24:00 +0100

    modify unit test with AssignReindeerToReindeerDto

---
+++ ClassLibrary1/DTOs/AssignReindeerToRouteRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Convidad.TechnicalTest.Models.DTOs;
+public record AssignReindeerToRouteRequest(
+    Guid ReindeerId,
+    int MaxDeliveries = 10
+);
